a38f03ac6ecfd1761ae3dbdbac1fd74197c7a063
Marin Sokol <marinsokol5@gmail.com>
2019-06-01 10:56:55 +0200

    bla

--- LSTMs_training.py
+++ LSTMs_training.py
@@ -231,7 +231,7 @@ if __name__ == '__main__':
     )
     # num_train_optimization_steps = int(len(train_data) / batch_size) * max_epochs
 
-    optimizer = Adam(lstms_model.parameters(), lr=0.001)
+    optimizer = Adam(lstms_model.parameters(), lr=0.0001)
     scheduler = StepLR(optimizer, step_size=30, gamma=0.1)
 
     weights = torch.Tensor([0.3, 0.7]).to(device)
