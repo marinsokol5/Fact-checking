319b897b5aec2f3861f1b951edffdd234657c9b3
Marin Sokol <marinsokol5@gmail.com>
2019-06-06 09:21:08 +0200

    synchronization with server

--- LSTMs_training.py
+++ LSTMs_training.py
@@ -218,7 +218,6 @@ if __name__ == '__main__':
 
     embedding_size = train_data[0][0].size()[1]
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    device = "cpu"
 
     visdom_vision = visdom.Visdom()
 
